f345bfb95a1ab8f7320b748880e9598abdc1e167
Marcus Pinnecke <marcus.pinnecke@gmail.com>
2017-12-09 02:00:27 +0100

    no message

--- docker_scan_exp/rscript.R
+++ docker_scan_exp/rscript.R
@@ -19,11 +19,12 @@ reverselog_trans <- function() {
                         domain = c(1e-100, Inf))
 }
 
-docker_17er <- read.csv(file="/Users/marcus/git/gecko/gecko-misc/docker_scan_exp/results/results_docker_17er.csv",head=TRUE,sep=";")
-#native_17er <- read.csv(file="/Users/marcus/git/gecko/gecko-misc/docker_scan_exp/results/results_plain_17er.csv",head=TRUE,sep=";")
-
-#docker_mac <- read.csv(file="/Users/marcus/git/gecko/gecko-misc/docker_scan_exp/results/results_docker_mac.csv",head=TRUE,sep=";")
 native_17er <- read.csv(file="/Users/marcus/git/gecko/gecko-misc/docker_scan_exp/results/results_plain_17er.csv",head=TRUE,sep=";")
+docker_17er <- read.csv(file="/Users/marcus/git/gecko/gecko-misc/docker_scan_exp/results/results_docker_17er.csv",head=TRUE,sep=";")
+
+native_mac <- read.csv(file="/Users/marcus/git/gecko/gecko-misc/docker_scan_exp/results/results_plain_mac.csv",head=TRUE,sep=";")
+docker_mac <- read.csv(file="/Users/marcus/git/gecko/gecko-misc/docker_scan_exp/results/results_docker_mac.csv",head=TRUE,sep=";")
+
 
 
 mean_up = function (data_source) {
@@ -33,30 +34,36 @@ mean_up = function (data_source) {
   return(agg_data)
 }
 
-agg_native = mean_up(native_17er)
-agg_docker = mean_up(docker_17er)
+agg_native_17er = mean_up(native_17er)
+agg_docker_17er = mean_up(docker_17er)
+agg_native_mac = mean_up(native_mac)
+agg_docker_mac = mean_up(docker_mac)
+
+
+len = length(agg_native$Group.1)
 
 agg_data = data.frame(
-  x = agg_native$Group.1,
-  y_native = sapply(agg_native$x, function(x) { x / 2048 }), 
-  y_docker = sapply(agg_docker$x, function(x) { x / 2048 })
+  x = c(agg_native$Group.1,agg_native$Group.1,agg_native$Group.1,agg_native$Group.1),
+  y = c(sapply(agg_native_17er$x, function(x) { x / 2048 }), 
+        sapply(agg_docker_17er$x, function(x) { x / 2048 }),
+        sapply(agg_native_mac$x, function(x) { x / 2048 }),
+        sapply(agg_docker_mac$x, function(x) { x / 2048 })),
+  group = c(rep("native 17er", len),
+            rep("docker 17er", len),
+            rep("native mac", len),
+            rep("docker macr", len))
 )
 
 
-plot(x = agg_data$x, y = agg_data$Group.1, type="l")
-
-ggplot(agg_data, aes(
-  x=(agg_data$x))) +
-  geom_line(aes(y = agg_data$y_native, colour = "native")) + 
-  geom_line(aes(y = agg_data$y_docker, colour = "docker")) + 
-  #scale_y_continuous(limits = c(1.6, 4.2), expand = c(0,0))+
-  #scale_x_continuous(limits = c(0,1024)) + 
-  scale_x_continuous(breaks=agg_data$x, labels=sapply(agg_data$x, function(x) { return('^'(2, x)) })) + 
-  xlab("number threads spawned") + 
-  ylab("request latency [msec/request]") + 
-  ggtitle("Overhead Application Dockerization (2048 identical requests with selectivity of 50% on 4.13 GiB column)") +
-  theme_bw()  + theme(
-    plot.title = element_text(size=10),
-    axis.title.x = element_text(size=10),
-    axis.title.y = element_text(size=10)
-  ) 
+ggplot(agg_data, aes(x=x, y=y, group=group)) +
+         geom_line(aes(linetype=group)) +
+         geom_point(aes(shape=group)) +
+        scale_x_continuous(breaks=agg_data$x, labels=sapply(agg_data$x, function(x) { return('^'(2, x)) })) + 
+        xlab("number threads spawned") + 
+        ylab("request latency [msec/request]") + 
+        ggtitle("Overhead Application Dockerization (2048 identical requests with selectivity of 50% on 4.13 GiB column)") +
+        theme_bw()  + theme(
+          plot.title = element_text(size=10),
+          axis.title.x = element_text(size=10),
+          axis.title.y = element_text(size=10)
+        ) 
