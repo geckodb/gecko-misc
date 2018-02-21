0e81bd21fc02f72de304c51e01da67dd30cd76f2
Marcus Pinnecke <marcus.pinnecke@gmail.com>
2018-02-21 08:39:10 +0100

    Update experiment script

--- string-db-gen/sbin/bin_width_exp.sh
+++ string-db-gen/sbin/bin_width_exp.sh
@@ -5,18 +5,21 @@ TARGET_DATASIZE_BYTE=52428800
 
 rm output/*
 
-SCENARIO_NAME=base
+SCENARIOS='base social synth'
 
 for ((sample=0; sample < 25 ; sample++))
 do  
 	for ((i=1; i <= 10000 ; i *= 10))
 	do  
-    	BIN_WIDTH=$i
-    	PROFILE_NAME="$SCENARIO_NAME-profile-w-$BIN_WIDTH"
-		java -jar build/string-db-gen.jar --clean-cache -w $BIN_WIDTH -a dewiki-dataset/files/dewiki-articles-word-freq.csv -b dewiki-dataset/files/dewiki-articles-next-words.txt -c dewiki-dataset/files/dewiki-articles-starter-words.csv -d dewiki-dataset/files/dewiki-articles-lengths.txt -s ${SCENARIO_NAME} -t ${TARGET_DATASIZE_BYTE} -o output/${SCENARIO_NAME}.csv -i "output/statistics.csv" -u $PROFILE_NAME && cat output/statistics.csv
+		for SCENARIO in $SCENARIOS
+    	do 
+    		BIN_WIDTH=$i
+    		SCENARIO_NAME=$SCENARIO
+    		PROFILE_NAME="$SCENARIO_NAME-profile-w-$BIN_WIDTH"
+			java -jar build/string-db-gen.jar --clean-cache -w $BIN_WIDTH -a dewiki-dataset/files/dewiki-articles-word-freq.csv -b dewiki-dataset/files/dewiki-articles-next-words.txt -c dewiki-dataset/files/dewiki-articles-starter-words.csv -d dewiki-dataset/files/dewiki-articles-lengths.csv -e dewiki-dataset/files/twitter-msg-lengths.csv -s ${SCENARIO_NAME} -t ${TARGET_DATASIZE_BYTE} -o output/${SCENARIO_NAME}.csv -i "output/statistics.csv" -u $PROFILE_NAME && cat output/statistics.csv
+		done			
 	done
 done
 
 
 
-#java -jar build/string-db-gen.jar --clean-cache -w $BIN_WIDTH -a dewiki-dataset/files/dewiki-articles-word-freq.csv -b dewiki-dataset/files/dewiki-articles-next-words.txt -c dewiki-dataset/files/dewiki-articles-starter-words.csv -d dewiki-dataset/files/dewiki-articles-lengths.txt -s ${SCENARIO_NAME} -t ${TARGET_DATASIZE_BYTE} -o output/${SCENARIO_NAME}.csv -i "output/statistics.csv" -u PROFILE_NAME && cat output/statistics.csv
